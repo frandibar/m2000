b0a996c8656a9064ce889e50db41d30c1d7816d7
Francisco Dibar <frandibar@gmail.com>
2011-12-27 21:52:14 -0300

    trivialidades

--- update-win.bat
+++ update-win.bat
@@ -7,5 +7,4 @@ easy_install m2000-master-py2.7.egg
 
 echo Creando acceso directo...
 xxmklink "%HOMEPATH%\Escritorio\m2000.lnk" %HOMEDRIVE%\Python27\python.exe %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\main.py %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\ "Mujeres 2000 - Sistema de Gestion de Creditos" 1 %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\m2000\art\win.ico /q
-
-xxmklink "%HOMEPATH%\Desktop\m2000.lnk" %HOMEDRIVE%\Python27\python.exe %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\main.py %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\ "Mujeres 2000 - Sistema de Gestion de Creditos" 1 %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\m2000\art\win.ico /q
+xxmklink "%HOMEPATH%\Desktop\m2000.lnk"    %HOMEDRIVE%\Python27\python.exe %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\main.py %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\ "Mujeres 2000 - Sistema de Gestion de Creditos" 1 %HOMEDRIVE%\Python27\Lib\site-packages\m2000-master-py2.7.egg\m2000\art\win.ico /q
