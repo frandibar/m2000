75bd65528a8afd5d70c62bd159b9a14afd6d8140
Francisco Dibar <frandibar@gmail.com>
2012-06-16 13:16:52 -0300

    new release

--- update-win.bat
+++ update-win.bat
@@ -2,16 +2,18 @@ echo Actualizacion de m2000
 echo Borrando archivo de configuracion para recrear DB...
 del "%HOMEPATH%\.m2000.cfg"
 
+set version=12.6.16.6
+
 echo Instalando m2000...
-easy_install m2000-12.4.23-py2.7.egg
+easy_install m2000-%version%-py2.7.egg
 
 echo Creando acceso directo...
-xxmklink "%HOMEPATH%\Escritorio\m2000.lnk" %HOMEDRIVE%\Python27\python.exe %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\main.py %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\ "Mujeres 2000 - Sistema de Gestion de Creditos" 1 %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\m2000\art\win.ico /q
-xxmklink "%HOMEPATH%\Desktop\m2000.lnk"    %HOMEDRIVE%\Python27\python.exe %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\main.py %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\ "Mujeres 2000 - Sistema de Gestion de Creditos" 1 %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\m2000\art\win.ico /q
+xxmklink "%HOMEPATH%\Escritorio\m2000.lnk" %HOMEDRIVE%\Python27\python.exe %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\main.py %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\ "Mujeres 2000 - Sistema de Gestion de Creditos" 1 %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\m2000\art\win.ico /q
+xxmklink "%HOMEPATH%\Desktop\m2000.lnk"    %HOMEDRIVE%\Python27\python.exe %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\main.py %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\ "Mujeres 2000 - Sistema de Gestion de Creditos" 1 %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\m2000\art\win.ico /q
 
-echo Aplicando parche a version 11.12.30 de camelot.
+echo Copiando archivos adicionales...
+copy firma-tesorero.png   %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\m2000\media
+copy firma-presidente.png %HOMEDRIVE%\Python27\Lib\site-packages\m2000-%version%-py2.7.egg\m2000\media
+
+echo Aplicando parche a version 11.12.30 de camelot...
 copy base.py %HOMEDRIVE%\Python27\Lib\site-packages\Camelot-11.12.30-py2.7.egg\camelot\admin\action\
-
-echo Copiando archivos adicionales.
-copy firma-tesorero.png   %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\m2000\media
-copy firma-presidente.png %HOMEDRIVE%\Python27\Lib\site-packages\m2000-12.4.23-py2.7.egg\m2000\media
